4e6ce792ee582ecb91ba76e42cc94b0ecc368239
Bibekananda25 <83566575+Bibekananda25@users.noreply.github.com>
2025-10-08 11:21:13 +0530

    6th Commit:- insert code for config package

--- src/test/java/Config/pojo.java
+++ src/test/java/Config/pojo.java
@@ -2,4 +2,70 @@ package Config;
 
 public class pojo {
 
+	    private int roomid;
+	    private String firstname;
+	    private String lastname;
+	    private boolean depositpaid;
+	    private Bookingdates bookingDates;
+	    private String email;
+	    private String phone;
+	    private String token;
+		private String endPoint;
+		private int bookingId;
+
+	    public pojo() {}
+
+	    public int getRoomid() { return roomid; }
+	    public void setRoomid(int roomid) { this.roomid = roomid; }
+
+	    public String getFirstname() { return firstname; }
+	    public void setFirstname(String firstname) { this.firstname = firstname; }
+
+	    public String getLastname() { return lastname; }
+	    public void setLastname(String lastname) { this.lastname = lastname; }
+
+	    public boolean isDepositpaid() { return depositpaid; }
+	    public void setDepositpaid(boolean depositpaid) { this.depositpaid = depositpaid; }   
+
+	    public String getEmail() { return email; }
+	    public void setEmail(String email) { this.email = email; }
+	    
+	    public Bookingdates getBookingdates() {
+	        return bookingDates;
+	    }
+
+	    public void setBookingdates(Bookingdates bookingdates) {
+	        this.bookingDates = bookingdates;
+	    }
+
+	    public String getPhone() { return phone; }
+	    public void setPhone(String phone) { this.phone = phone; }
+	    
+		public void setEndPoint(String endpoint) {
+			endPoint = endpoint;
+		}
+		
+		public String getEndPoint() {
+			return endPoint;
+		}
+		
+		public void setToken(String tokenvalue) {
+			token = tokenvalue;
+		}
+		
+		public String getToken() {
+			return token;
+		}
+		
+		public void setBookingId(int bookingid) {
+			bookingId = bookingid;
+		}
+		
+		public int getBookingId() {
+			return bookingId;
+		}
+
+
 }
+	
+
