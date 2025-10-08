6f15082894fe03558d4b7f7735f7478dce63c23a
Bibekananda25 <83566575+Bibekananda25@users.noreply.github.com>
2025-10-08 11:40:13 +0530

    7th Commit:- configure TestRunner file successfully

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,8 +1,25 @@
 package TestRunner;
 
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features",
+
+		glue = "stepdefinitions", dryRun=false,
+
+		plugin = {"pretty", // pretty-printed logs in console
+	    "summary", // clean summary at the end
+	    "html:target/cucumber-report.html", // HTML report
+	    "json:target/cucumber-report.json" // JSON report
+	     },
+         monochrome = true
+
+)
 
 public class TestRunner {
 
